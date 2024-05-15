f9c121ac2362369a94021a04cc06b257be98370b
Camila <camila.rodriguez.aguila@gmail.com>
2024-05-15 09:15:16 -0700

    Añadir pedir al usuario la primera matriz

--- Tarea3/main.java
+++ Tarea3/main.java
@@ -6,20 +6,25 @@ public class main {
     public static void main(String[] args) {
 
         // Instancias
-        Matriz matriz1 = new Matriz(3, 2);
         Funciones funciones = new Funciones();
         Scanner sc = new Scanner(System.in);
 
-        // Multiplicando las matrices
-        int filas = Funciones.obtenerNumero(sc, "Hola! \nIngrese el número de filas: ");
-        int columnas = Funciones.obtenerNumero(sc, "Ingrese el número de columnas: ");
+        // Pedir datos de las matrices
+        System.out.println("=== Matriz 1 ===");
+        int filas1 = Funciones.obtenerNumero(sc, "Hola! \nIngrese el número de filas: ");
+        int columnas1 = Funciones.obtenerNumero(sc, "Ingrese el número de columnas: ");
+
+        System.out.println("\n=== Matriz 2 ===");
+        int filas2 = Funciones.obtenerNumero(sc,  "Ingrese el número de filas: ");
+        int columnas2 = Funciones.obtenerNumero(sc, "Ingrese el número de columnas: ");
 
         // Imprimiendo las matrices y el resultado
         System.out.println("Matriz 1:");
+        Matriz matriz1 = new Matriz(filas1,columnas1);
         matriz1.imprimirMatriz();
 
         System.out.println("\nMatriz 2:");
-        Matriz matriz2 = new Matriz(filas, columnas);
+        Matriz matriz2 = new Matriz(filas2, columnas2);
         matriz2.imprimirMatriz();
 
         System.out.println("\nResultado de la multiplicación:");
